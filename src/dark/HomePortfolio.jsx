22908dffe35667751a4f9df0f44275f9337795a5
ed1868 <ruizeduardo21@gmail.com>
2021-04-22 00:53:39 -0400

    section five done

--- src/dark/HomePortfolio.jsx
+++ src/dark/HomePortfolio.jsx
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from "react";
 import ScrollToTop from 'react-scroll-up';
-import { FiChevronUp } from "react-icons/fi";
 import PortfolioMasonry from "../elements/portfolio/PortfolioMasonry";
 import Header from "../component/header/Header";
 import FooterTwo from "../component/footer/FooterTwo";
@@ -11,6 +10,21 @@ import ServiceTwo from "../elements/service/ServiceTwo";
 import Particles from 'react-particles-js';
 import { FiActivity, FiCast, FiMap } from "react-icons/fi";
 
+import {
+    FaReact,
+    FaSass
+} from "react-icons/fa";
+import {
+    FiSmartphone,
+    FiCode,
+    FiDownload,
+    FiCommand,
+    FiHeadphones,
+    FiBold,
+    FiChevronUp
+} from "react-icons/fi";
+
+
 const SlideList = [
     {
         textPosition: 'text-center',
@@ -19,6 +33,49 @@ const SlideList = [
         description: 'Take Control of your Digital Footprint. Everywhere you go',
         buttonText: 'Contact Us',
         buttonLink: '/contact'
+    }
+]
+
+const featureList = [
+    {
+        icon: <FaReact/>,
+        title: 'Latest React Bundle',
+        subtitle: 'We use the latest React framework version for a clean user friendly interface. '
+    },
+    {
+        icon: <FiSmartphone/>,
+        title: 'Perfect Responsive',
+        subtitle: 'Our apps and websites are Perfect for all devices.'
+    },
+    {
+        icon: <FiCode/>,
+        title: 'Blockchain and Ethereum',
+        subtitle: 'We have implemented Ethreum protocols with Solidity and Web3.js to give our clients a head start on blockchain technology.'
+    },
+    {
+        icon: <FaSass/>,
+        title: 'Sass',
+        subtitle: 'We use Sass in every project to give logic to our styles.'
+    },
+    {
+        icon: <FiDownload/>,
+        title: 'Fast Loading Speed',
+        subtitle: 'Our team is fast. Our Artificial Intelligence models? Even Faster '
+    },
+    {
+        icon: <FiCommand/>,
+        title: 'Modern Design',
+        subtitle: 'AI Nomads is a modern creative team that doesnt get with the times. It goes beyond them.'
+    },
+    {
+        icon: <FiHeadphones/>,
+        title: '24 Support System',
+        subtitle: 'We provide 24 hour support for all clients. We are one click away.'
+    },
+    {
+        icon: <FiBold/>,
+        title: 'Bitcoin and alt coins',
+        subtitle: 'With blockchain we intergrate payment systems like Bitcoin and other Alt Coins.'
     }
 ]
 
@@ -211,8 +268,41 @@ class HomePortfolio extends Component {
                 </div>
                 {/* End Progress Bar Area   */}
 
+                                    {/* Start Feature Area  */}
+                                    <div id="feature" className="service-area bg_color--1 ptb--120">
+                        <div className="wrapper plr--120">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="section-title text-center pb--30">
+                                        <span className="subtitle">Intelligent Magic</span>
+                                        <br></br>
+                                        <h2 className="title theme-gradient">Our Awesome Features</h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row service-main-wrapper">
+                                {/* Start Single Feature  */}
+                                {featureList.map((value, i) => (
+                                    <div className="col-lg-6 col-xl-3 col-md-6 col-sm-6 col-12" key={i}>
+                                        <div className="service service__style--2 text-left">
+                                            <div className="icon">
+                                                {value.icon}
+                                            </div>
+                                            <div className="content">
+                                                <h3 className="title">{value.title}</h3>
+                                                <p className="subtitle">{value.subtitle}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                                {/* End Single Feature  */}
+                            </div>
+                        </div>
+                    </div>
+                    {/* End Feature Area  */}
+
                 {/* Start Brand Area */}
-                <div className="rn-brand-area pb--120 bg_color--10">
+                {/* <div className="rn-brand-area pb--120 bg_color--10">
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-12">
@@ -236,7 +326,7 @@ class HomePortfolio extends Component {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
                 {/* End Brand Area */}
 
                 {/* Start Footer Area  */}
