2216e89e16c431fab3e42361a2bde8bcdbce66eb
ed1868 <ruizeduardo21@gmail.com>
2021-04-21 21:14:53 -0400

    first full comitt

---
+++ src/component/common/Helmet.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import {Helmet} from 'react-helmet'
+
+class PageHelmet extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>{this.props.pageTitle} || AI Nomads  </title>
+                    <meta name="robots" content="noindex, follow" />
+                    <meta name="description" content="AI Nomads - a Company that uses Artificial Intelligence to help solve business problems in every sector." />
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                </Helmet>
+            </React.Fragment>
+        )
+    }
+}
+PageHelmet.propTypes = {
+    title: PropTypes.string
+};
+export default PageHelmet;
