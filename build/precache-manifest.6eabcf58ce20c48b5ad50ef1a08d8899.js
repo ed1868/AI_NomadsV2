f077d7ad4b9e693f425c066988ef75b7906d9877
ed1868 <ruizeduardo21@gmail.com>
2021-04-22 01:20:00 -0400

    version one complete and deployed

--- build/precache-manifest.6eabcf58ce20c48b5ad50ef1a08d8899.js
+++ build/precache-manifest.6eabcf58ce20c48b5ad50ef1a08d8899.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "702fbf8e97a4af342dbe23dd21b902b7",
+    "revision": "14a08f57920bc10475de270c63843a92",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.4a5788a2.chunk.css"
   },
   {
-    "revision": "7830910ff3f18754d4e0",
+    "revision": "88dee89a8779d9d3ea72",
     "url": "/static/css/main.f31541a7.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.bfef3d20.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7830910ff3f18754d4e0",
-    "url": "/static/js/main.8b2f331e.chunk.js"
+    "revision": "88dee89a8779d9d3ea72",
+    "url": "/static/js/main.eb6cf845.chunk.js"
   },
   {
     "revision": "208783fa02780d630bac",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03bad84419942f08fed491a364c50104.js"
+  "/precache-manifest.6eabcf58ce20c48b5ad50ef1a08d8899.js"
 );
 
 self.addEventListener('message', (event) => {
