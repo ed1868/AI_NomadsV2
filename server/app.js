f8df2e9f8556f9b3724f61e409793a3f3d20d381
ed1868 <ruizeduardo21@gmail.com>
2021-05-03 19:47:27 -0400

    the right server working with mongo,ready for intergration with react

--- server/app.js
+++ server/app.js
@@ -1,47 +1,87 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, '.env') })
 
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('morgan')
+const nocache = require('nocache')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
-const app = express();
+require('./configs/database')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app_name = require('./package.json').name
+const debug = require('debug')(
+  `${app_name}:${path.basename(__filename).split('.')[0]}`
+)
 
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+const app = express()
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(nocache())
 
-const index = require('./routes/index');
-app.use('/', index);
+// Set "Access-Control-Allow-Origin" header
+app.use(
+  cors({
+    origin: (origin, cb) => {
+      cb(null, process.env.NODE_ENV !== 'production')
+    },
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+)
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// Set the public folder to "~/client/build/"
+// Example: http://localhost:5000/favicon.ico => Display "~/client/build/favicon.ico"
+app.use(express.static(path.join(__dirname, '../client/build')))
 
-// error handler
+// Enable authentication using session + passport
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'irongenerator',
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+)
+require('./passport')(app)
+
+app.use('/api', require('./routes/index'))
+app.use('/api', require('./routes/auth'))
+app.use('/api/countries', require('./routes/countries'))
+
+// For any routes that starts with "/api", catch 404 and forward to error handler
+app.use('/api/*', (req, res, next) => {
+  let err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// For any other routes, redirect to the index.html file of React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+// Error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.error('----- An error happened -----')
+  console.error(err)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(err.status || 500)
 
-module.exports = app;
+    // A limited amount of information sent in production
+    if (process.env.NODE_ENV === 'production') res.json(err)
+    else
+      res.json(JSON.parse(JSON.stringify(err, Object.getOwnPropertyNames(err))))
+  }
+})
+
+module.exports = app
