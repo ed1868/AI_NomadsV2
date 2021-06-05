f2fd1321f92bc70ecd798a72db02333253c06a76
ed1868 <ruizeduardo21@gmail.com>
2021-06-05 19:40:31 -0400

    edit tokenList model to put confirmation code

--- server/models/TokenWaitList.js
+++ server/models/TokenWaitList.js
@@ -1,22 +1,27 @@
 const mongoose = require('mongoose')
 
 const tokenWaitListSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: [true, 'Your First name is required'],
-  },
-  email: {
-    type: String,
-    require:true,
-  },
-  confirmed: {
-    type: String,
-    enum:['confirmed','not confirmed'],
-    default:'not confirmed'
-  },
-  approved: {
-    type: String,
-  },
+    firstName: {
+        type: String,
+        required: [true, 'Your First name is required'],
+    },
+    email: {
+        type: String,
+        require: true,
+    },
+    confirmationCode: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    confirmed: {
+        type: String,
+        enum: ['confirmed', 'not confirmed'],
+        default: 'not confirmed'
+    },
+    approved: {
+        type: String,
+    },
 })
 
 const TokenWaitList = mongoose.model('TokenWaitList', tokenWaitListSchema)
